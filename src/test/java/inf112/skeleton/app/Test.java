9a6004d32040b1a4ed221801ee52eae903e59ef3
Linh Nguyen <Linh.Nguyen@uib.no>
2019-02-04 19:48:05 +0100

    test

--- src/test/java/inf112/skeleton/app/Test.java
+++ src/test/java/inf112/skeleton/app/Test.java
@@ -2,7 +2,7 @@ package inf112.skeleton.app;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test 2");
     }
 
 }
