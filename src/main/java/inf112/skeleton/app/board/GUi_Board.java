05fed2b141ee5015867573fd1fead7a59a8684ef
Even Kolsgaard <even.kolsgaard@student.uib.no>
2019-02-06 17:54:14 +0100

    first commit

---
+++ src/main/java/inf112/skeleton/app/board/GUi_Board.java
@@ -0,0 +1,50 @@
+package inf112.skeleton.app.board;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GUi_Board implements ApplicationListener {
+    private SpriteBatch batch;
+    private Sprite sprite;
+    private Texture texture;
+    
+    
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal("Assets/board1.png"));
+        sprite = new Sprite(texture);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+}
