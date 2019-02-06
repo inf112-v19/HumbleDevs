2b051e0103476b0e511d3db5a622cc82dcf891ab
Even Kolsgaard <even.kolsgaard@student.uib.no>
2019-02-06 18:00:43 +0100

    draw board, test

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,14 +3,15 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import inf112.skeleton.app.board.GUi_Board;
+
 
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "hello-world";
-        cfg.width = 480;
-        cfg.height = 320;
-
-        new LwjglApplication(new HelloWorld(), cfg);
+        cfg.width = 1800;
+        cfg.height = 1800;
+        new LwjglApplication(new GUi_Board(), cfg);
     }
 }
