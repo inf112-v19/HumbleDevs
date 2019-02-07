4ce7bf310774c0004a62aa318ecc4b80297bda67
Amalie Helleberg Rovik <Amalie.Rovik@student.uib.no>
2019-02-07 10:49:34 +0100

    example test

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "hello-world";
+        cfg.title = "hello-World";
         cfg.width = 480;
         cfg.height = 320;
 
