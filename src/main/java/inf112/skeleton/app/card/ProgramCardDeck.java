f2a410f0087022f5e7d24dd355276e07ce34a887
Linh Nguyen <Linh.Nguyen@uib.no>
2019-02-21 11:53:04 +0100

    update class

--- src/main/java/inf112/skeleton/app/card/ProgramCardDeck.java
+++ src/main/java/inf112/skeleton/app/card/ProgramCardDeck.java
@@ -19,28 +19,30 @@ public class ProgramCardDeck implements IDeck{
         }
 
         for(int i = 36; i < 42; i ++) {   //rotate 180
-            moveCard.add(new ProgramCard(10+10*(i-36), 0, 2 ));
+            moveCard.add(new ProgramCard(10+10*i, 0, 2 ));
         }
 
         for(int i = 42; i < 60; i ++) { //move forward 1
-            moveCard.add(new ProgramCard(490+20*(i-42), 1, 0 ));
+            moveCard.add(new ProgramCard(490+20*i-42, 1, 0 ));
         }
 
         for(int i = 60; i < 72; i ++) { //move forward 2
-            moveCard.add(new ProgramCard(670+10*(i-60), 2, 0 ));
+            moveCard.add(new ProgramCard(670+10*i, 2, 0 ));
         }
 
         for(int i = 72; i < 78; i ++) { //move forward 13
-            moveCard.add(new ProgramCard(790+10*(i-72), 3, 0 ));
+            moveCard.add(new ProgramCard(790+10*i, 3, 0 ));
         }
 
         for(int i = 78; i < 84; i ++) { //move backward
-            moveCard.add(new ProgramCard(430+10*(i-78), -1, 0 ));
+            moveCard.add(new ProgramCard(430+10*i-78, -1, 0 ));
         }
 
         Collections.shuffle(moveCard);
 
     }
+    
+
 
 
     @Override
