4dc0ea25aac3b4f8462ad5566b6fc3a76e3ea44e
Joachim Morken <joknumo@gmail.com>
2019-03-06 15:52:25 +0100

    added arraylist for each position xy to store objects

--- src/main/java/inf112/skeleton/app/game/Main.java
+++ src/main/java/inf112/skeleton/app/game/Main.java
@@ -7,6 +7,7 @@ import inf112.skeleton.app.graphics.Tiled;
 
 
 public class Main {
+
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "hello-robot";
---
+++ src/main/java/inf112/skeleton/app/graphics/Grid.java
@@ -0,0 +1,37 @@
+package inf112.skeleton.app.graphics;
+
+import com.badlogic.gdx.maps.MapObjects;
+import inf112.skeleton.app.gameObjects.IItem;
+import com.badlogic.gdx.maps.tiled.*;
+import java.util.ArrayList;
+
+
+public class Grid {
+    private int WIDTH = 0;
+    private int HEIGHT = 0;
+    private TiledMapTileLayer baseLayer;
+    private TiledMapTileLayer playerLayer;
+    private TiledMapTileLayer objectLayer;
+    private ArrayList grid[][];
+
+    public Grid(TiledMap map) {
+        this.WIDTH = (int) map.getProperties().get("width");
+        this.HEIGHT = (int) map.getProperties().get("height");
+        //grid = new int[WIDTH][HEIGHT];
+        baseLayer = (TiledMapTileLayer) map.getLayers().get(0);
+        playerLayer = (TiledMapTileLayer) map.getLayers().get(1);
+        objectLayer = (TiledMapTileLayer) map.getLayers().get(2);
+        grid = new ArrayList[WIDTH][HEIGHT];
+
+        for (int r = 0; r < HEIGHT; r++) {
+            for (int c = 0; c < WIDTH; c++) {
+                // Each cell has a list of items/objects
+                ArrayList<IItem> list = new ArrayList<>();
+                grid[r][c] = list;
+
+            }
+        }
+
+
+    }
+}
