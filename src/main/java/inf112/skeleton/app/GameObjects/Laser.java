b79d224b4a9f081239987de932ab86d29c79d1cb
Amalie Helleberg Rovik <Amalie.Rovik@student.uib.no>
2019-03-06 18:21:26 +0100

    started on Laser class

---
+++ src/main/java/inf112/skeleton/app/GameObjects/Laser.java
@@ -0,0 +1,28 @@
+package inf112.skeleton.app.GameObjects;
+
+import inf112.skeleton.app.board.Direction;
+
+public class Laser implements IItem{
+
+    private Direction dir;
+
+    @Override
+    public IItem getType() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "Laser";
+    }
+
+    @Override
+    public char getSymbol() {
+        return '-';
+    }
+
+    @Override
+    public Direction getDirection() {
+        return dir;
+    }
+}
