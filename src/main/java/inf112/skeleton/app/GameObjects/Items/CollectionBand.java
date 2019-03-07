0ac528046db16d98d08478f5f89e63d787eecbfd
Amalie Helleberg Rovik <Amalie.Rovik@student.uib.no>
2019-03-07 12:33:18 +0100

    reuncommented tests

--- src/main/java/inf112/skeleton/app/GameObjects/Items/CollectionBand.java
+++ src/main/java/inf112/skeleton/app/GameObjects/Items/CollectionBand.java
@@ -1,4 +1,40 @@
 package inf112.skeleton.app.GameObjects.Items;
 
-public class collectionBand {
+/**
+ * CollectionBand that gives info to the game class on the movement and possibly rotation.
+ * @author Amalie Rovik
+ */
+
+public class CollectionBand implements IItem {
+
+    private int movement;
+    private int rotation;
+
+    public CollectionBand(int movement, int rotation) {
+        this.movement = movement;
+        this.rotation = rotation;
+    }
+
+    @Override
+    public int tileId() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "CollectionBand";
+    }
+
+    @Override
+    public char getSymbol() {
+        return '_';
+    }
+
+    public int getMovement() {
+        return movement;
+    }
+
+    public int rotation() {
+        return rotation;
+    }
 }
