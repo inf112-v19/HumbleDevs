78e54fcf008f8b0f40a6dd04ac47f13d8f558b23
Amalie Helleberg Rovik <Amalie.Rovik@student.uib.no>
2019-03-07 12:20:44 +0100

    made collectionband

--- src/test/java/inf112/skeleton/app/GameTest.java
+++ src/test/java/inf112/skeleton/app/GameTest.java
@@ -21,7 +21,7 @@ public class GameTest {
 		ArrayList<String> test = new ArrayList<String>();
 		ProgramCard[] tet = new ProgramCard[9];
 		for(int x = 0; x < 9; x++) {
-			tet[x] = new ProgramCard(x);
+			//tet[x] = new ProgramCard(x);
 		}
 		Board hor = new Board(12, 12,test);
 		Game fysj = new Game(hor, tet, 3);
