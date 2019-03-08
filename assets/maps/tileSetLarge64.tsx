d52554425f4c710b8497e2b57616d893ea8c16a1
Bendik Sem Kvernevik <bendik.kvernevik@student.uib.no>
2019-03-08 13:42:29 +0100

    Fix asset folder

---
+++ assets/maps/tileSetLarge64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tileSetLarge64" tilewidth="64" tileheight="64" tilecount="96" columns="6">
+ <image source="../../texture/tileSetLarge64.png" width="384" height="1024"/>
+</tileset>
