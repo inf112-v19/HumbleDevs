74915c425205a26153098f2d1a1610e6a0f396ca
Joachim Morken <joknumo@gmail.com>
2019-04-01 12:27:17 +0200

    Started on GUI tests

---
+++ src/test/java/inf112/skeleton/app/GameScreenTest.java
@@ -0,0 +1,19 @@
+package inf112.skeleton.app;
+
+import inf112.skeleton.app.graphics.GUI;
+import inf112.skeleton.app.graphics.Screens.GameScreen;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class GameScreenTest {
+
+    @Test
+    public void gameScreenPresentsCards () {
+        GUI gui = new GUI();
+
+    }
+
+
+}
