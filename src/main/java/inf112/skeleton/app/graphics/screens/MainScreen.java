0093a53714a2f0e9e84403b82b0248d86a2c9969
Joachim Morken <joknumo@gmail.com>
2019-04-11 10:54:55 +0200

    Started on mainscreen

---
+++ src/main/java/inf112/skeleton/app/graphics/screens/MainScreen.java
@@ -0,0 +1,22 @@
+package inf112.skeleton.app.graphics.screens;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Screen;
+
+public class MainScreen extends ApplicationAdapter implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
